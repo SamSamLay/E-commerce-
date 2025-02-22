484e32e0d1e0a9b03eaf0b4e787052208ce6d241
[Sam Sam] <[samsam67315@gmail.com]>
2025-02-23 01:30:36 +0630

    Move right

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     @vite(['resources/css/app.css'])
 </head>
 <body class="bg-gray-100">
@@ -67,7 +68,20 @@
   </div>
 </div>
 
-
+<div class="relative flex flex-col my-6 bg-white shadow-sm border border-slate-200 rounded-lg w-96 ml-auto">
+  <div class="p-4 bg-teal-50">
+    <h5 class="mb-2 text-slate-800 text-xl font-semibold">
+      Website Review Check Update from Our Team in San Francisco
+    </h5>
+    <p class="text-slate-600 leading-normal font-light">
+      The place is close to Barceloneta Beach and bus stop just 2 min by walk and near to Naviglio where you can enjoy the main night life in Barcelona.
+    </p>
+ 
+    <button class="rounded-md bg-slate-800 py-2 px-4 mt-6 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" type="button">
+      Read more
+    </button>
+  </div>
+</div>
 
 
 {{-- -carousel photo --}}
