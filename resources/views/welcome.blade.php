b0df2057f0a7b7e90b00395bec25c90187a879a8
[Sam Sam] <[samsam67315@gmail.com]>
2025-02-26 11:50:06 +0630

    hover button without icon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -148,7 +148,16 @@ Our Products
                 <div class="absolute top-2 right-2 bg-red-500 text-white text-sm font-bold w-12 h-12 flex items-center justify-center rounded-full">
                     -30%
                 </div>
-                <button class="btn border-2 text-green-500 bg-white border-green-500 px-8 py-1">Add to cart</button>
+                <div class="button-group">
+                    <button class="btn border-2 text-green-500 bg-white border-green-500 px-8 py-1">Add to cart</button>
+                    <div class="second-row">
+                    <button class="btn text-white"><i class="fas fa-share text-white"></i>Share</button>
+                    <button class="btn text-white"><i class="fas fa-exchange-alt text-white"></i>Compare</button>
+                    <button class="btn text-white"><i class="fas fa-heart text-white"></i>Like</button>
+                    </div>
+                </div>
+                
+                
             </div>
             <div class="p-4">
                 <h3 class="text-lg font-semibold">Syltherine</h3>
