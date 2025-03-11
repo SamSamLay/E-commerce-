d00d932f1ce370bbadf4e6769c3879c6bd2ef31e
SamSamLay <samsam67315@gmail.com>
2025-03-11 13:29:25 +0630

    button clear

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
-</head>
-<body>
+<x-layout>
+  <x-nav-bar />
 <div class="w-[1440px] h-[900px] relative bg-white overflow-hidden">
   <div class="p-5 left-[80px] top-[677px] absolute bg-[#3aa29f]/10 rounded border border-[#3aa29f] justify-start items-start gap-3 inline-flex">
     <div data-svg-wrapper class="relative">
@@ -21,29 +14,30 @@
   <x-order-summary />
 
 
+  
   <!--Count data -->
   <form method="POST" action="">
-  @csrf
-        <div class="w-[820px] h-40 left-[80px] top-[257px] absolute">
-          <div class="h-[52px] p-4 left-[196px] top-[98px] absolute rounded border border-[#a2a3b1] justify-start items-center gap-10 inline-flex">
-            <div data-svg-wrapper class="relative">
-            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M4.16663 10H15.8333" stroke="#17183B" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-            </svg>
-            </div>
+  @csrf 
+        <div class="w-[820px] h-40 left-[80px] top-[257px] absolute inline-flex shadow-xs" role="group">
+          <!--button +--->
+          <div class="h-[52px] p-4 left-[196px] top-[98px] absolute rounded justify-start items-center inline-flex">
+            
+            <button id="minusBtn1" type="button" class="px-4 py-2 text-lg font-medium text-gray-900 bg-white border border-gray-200 rounded-s-lg hover:bg-gray-100 hover:text-blue-700 ">
+            -
+            </button>
+           
 
             <!--1-->
-            <div class="text-[#17183b] text-xl font-semibold font-['Proxima Nova'] leading-normal tracking-tight">1</div>
-            <div data-svg-wrapper class="relative">
-            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M10 4.16666V15.8333" stroke="#17183B" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-            <path d="M4.16663 10H15.8333" stroke="#17183B" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-            </svg>
-            </div>
+            <span  id="textDisplay1" class="px-4 py-2 text-lg font-medium text-gray-900 bg-white border-t border-b border-gray-200">
+                0
+            </span>
+            
+            <button id="plusBtn1" type="button" class="px-4 py-2 text-lg font-medium text-gray-900 bg-white border border-gray-200 rounded-e-lg hover:bg-gray-100 hover:text-blue-700">
+                +
+            </button>
+            
           </div>
-          <div class="h-5 left-[384px] top-[114px] absolute rounded justify-center items-center gap-4 inline-flex">
-            <div class="text-[#e14b4b] text-base font-semibold font-['Proxima Nova'] leading-tight tracking-tight">Remove</div>
-          </div>
+          
           
           <div class="left-[745px] top-[10px] absolute text-right text-[#17183b] text-xl font-semibold font-['Proxima Nova'] leading-normal tracking-tight">$149.99</div>
           <div class="left-[248px] top-[50px] absolute text-[#17183b] text-base font-semibold font-['Proxima Nova'] leading-tight tracking-tight">Gunnared biege</div>
@@ -54,24 +48,25 @@
 
         <div class="w-[820px] h-px left-[80px] top-[449px] absolute bg-[#d1d1d8]"></div>
   
-  <div class="w-[820px] h-40 left-[80px] top-[482px] absolute">
-    <div class="h-5 left-[384px] top-[114px] absolute rounded justify-center items-center gap-4 inline-flex">
-      <div class="text-[#e14b4b] text-base font-semibold font-['Proxima Nova'] leading-tight tracking-tight">Remove</div>
-    </div>
-    <div class="h-[52px] p-4 left-[196px] top-[98px] absolute rounded border border-[#a2a3b1] justify-start items-center gap-10 inline-flex">
-      <div data-svg-wrapper class="relative">
-      <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M4.16663 10H15.8333" stroke="#17183B" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-      </svg>
-      </div>
-      <div class="text-[#17183b] text-xl font-semibold font-['Proxima Nova'] leading-normal tracking-tight">1</div>
-      <div data-svg-wrapper class="relative">
-      <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M10 4.16669V15.8334" stroke="#17183B" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-      <path d="M4.16663 10H15.8333" stroke="#17183B" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-      </svg>
-      </div>
-    </div>
+        <div class="w-[820px] h-40 left-[80px] top-[482px] absolute">
+    
+  
+          <!--button +--->
+          <div class="h-[52px] p-4 left-[196px] top-[98px] absolute rounded justify-start items-center inline-flex">
+            
+            <button id="minusBtn1" type="button" class="px-4 py-2 text-lg font-medium text-gray-900 bg-white border border-gray-200 rounded-s-lg hover:bg-gray-100 hover:text-blue-700 ">
+            -
+            </button>
+            <span  id="textDisplay1" class="px-4 py-2 text-lg font-medium text-gray-900 bg-white border-t border-b border-gray-200">
+                0
+            </span>
+            
+            <button id="plusBtn1" type="button" class="px-4 py-2 text-lg font-medium text-gray-900 bg-white border border-gray-200 rounded-e-lg hover:bg-gray-100 hover:text-blue-700">
+                +
+            </button>
+            
+        </div>
+   
     <div class="left-[745px] top-[10px] absolute text-right text-[#17183b] text-xl font-semibold font-['Proxima Nova'] leading-normal tracking-tight">$169.99</div>
     <div class="left-[248px] top-[50px] absolute text-[#17183b] text-base font-semibold font-['Proxima Nova'] leading-tight tracking-tight">Lysed bright green</div>
     <div class="left-[196px] top-[50px] absolute text-[#a2a3b1] text-base font-semibold font-['Proxima Nova'] leading-tight tracking-tight">Color</div>
@@ -80,7 +75,7 @@
   </div>
   <div class="left-[80px] top-[153px] absolute text-[#17183b] text-[32px] font-bold font-['Proxima Nova'] leading-[44px]">Cart</div>
   <div class="left-[166px] top-[167px] absolute text-[#a2a3b1] text-xl font-semibold font-['Proxima Nova'] leading-normal">2 ITEMS</div>
-  <x-navbar />
+ 
 </div>
   </form>
 
@@ -89,5 +84,4 @@
   
 
 
-</body>
-</html>
+</x-layout>
